6e9149bea5b17b106675d596fd9a8ccab6414ab7
Jaakko Malkki <Jaakko.Malkki@hsl.fi>
2022-07-13 10:23:49 +0300

    Create functions for parsing GTFS times and dates

---
+++ src/test/kotlin/fi/hsl/gtfsrt2hfp/gtfs/utils/GtfsDateTimeParserTest.kt
@@ -0,0 +1,32 @@
+package fi.hsl.gtfsrt2hfp.gtfs.utils
+
+import fi.hsl.gtfsrt2hfp.fi.hsl.gtfsrt2hfp.gtfs.utils.parseGtfsDate
+import fi.hsl.gtfsrt2hfp.fi.hsl.gtfsrt2hfp.gtfs.utils.parseGtfsTime
+import org.junit.jupiter.api.assertThrows
+import java.time.LocalDate
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class GtfsDateTimeParserTest {
+    @Test
+    fun `Parse valid time`() {
+        val time = "10:15:15"
+
+        val seconds = parseGtfsTime(time)
+
+        assertEquals(36915, seconds)
+    }
+
+    @Test
+    fun `Parse invalid time`() {
+        assertThrows<IllegalArgumentException> { parseGtfsTime("fnsaiodnfa") }
+        assertThrows<IllegalArgumentException> { parseGtfsTime("a:b:c") }
+    }
+
+    @Test
+    fun `Parse valid date`() {
+        val date = parseGtfsDate("20220101")
+
+        assertEquals(LocalDate.of(2022, 1, 1), date)
+    }
+}
