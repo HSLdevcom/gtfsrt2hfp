f5decd48da3e3c5218aac71660cc9132448b7524
Jaakko Malkki <Jaakko.Malkki@hsl.fi>
2022-07-13 15:32:37 +0300

    Create extension functions for using MqttAsyncClient with coroutines

---
+++ src/main/kotlin/fi/hsl/gtfsrt2hfp/utils/mqttCoroutines.kt
@@ -0,0 +1,25 @@
+package fi.hsl.gtfsrt2hfp.fi.hsl.gtfsrt2hfp.utils
+
+import org.eclipse.paho.client.mqttv3.*
+import kotlin.coroutines.Continuation
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
+import kotlin.coroutines.suspendCoroutine
+
+private class ContinuationMqttActionListener(private val continuation: Continuation<Unit>) : IMqttActionListener {
+    override fun onSuccess(asyncActionToken: IMqttToken) {
+        continuation.resume(Unit)
+    }
+
+    override fun onFailure(asyncActionToken: IMqttToken, exception: Throwable) {
+        continuation.resumeWithException(exception)
+    }
+}
+
+suspend fun MqttAsyncClient.connectAsync(mqttConnectOptions: MqttConnectOptions) = suspendCoroutine<Unit> {
+    connect(mqttConnectOptions, null, ContinuationMqttActionListener(it))
+}
+
+suspend fun MqttAsyncClient.publishAsync(topic: String, message: MqttMessage) = suspendCoroutine<Unit> {
+    publish(topic, message, null, ContinuationMqttActionListener(it))
+}
