bd5a11750a40b55d6887d5fc86a4cbda55f1fd9b
Jaakko Malkki <Jaakko.Malkki@hsl.fi>
2022-07-14 15:15:37 +0300

    Create TripMatcher for matching trips between GTFS feeds

---
+++ src/main/kotlin/fi/hsl/gtfsrt2hfp/gtfs/matcher/TripMatcher.kt
@@ -0,0 +1,33 @@
+package fi.hsl.gtfsrt2hfp.fi.hsl.gtfsrt2hfp.gtfs.matcher
+
+import fi.hsl.gtfsrt2hfp.fi.hsl.gtfsrt2hfp.gtfs.utils.GtfsIndex
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+
+//TODO: extract interface
+class TripMatcher(private val gtfsIndexA: GtfsIndex, private val gtfsIndexB: GtfsIndex, private val routeMatcher: RouteMatcher) {
+    private val stopCodeStopMatcher = StopCodeStopMatcher(gtfsIndexA, gtfsIndexB)
+
+    fun matchTrip(tripId: String, startDate: String): String? {
+        val trip = gtfsIndexA.tripsById[tripId] ?: return null
+
+        val stopTimes = gtfsIndexA.stopTimesByTripId[trip.tripId]
+        val stopTimesInB = stopTimes?.mapNotNull { stopTime ->
+            val stopIdB = stopCodeStopMatcher.matchStop(stopTime.stopId).toSet()
+            if (stopIdB.isEmpty()) { null } else { stopIdB to stopTime.departureTime }
+        } ?: emptyList()
+
+        val routeIdA = trip.routeId
+        val routeIdB = routeMatcher.matchRoute(routeIdA).firstOrNull()
+
+        val startDateLocalDate = LocalDate.parse(startDate, DateTimeFormatter.ofPattern("yyyyMMdd"))
+
+        return gtfsIndexB.tripsById.values.find { trip ->
+            val stopTimes = gtfsIndexB.stopTimesByTripId[trip.tripId]!!
+            return@find trip.routeId == routeIdB
+                    && gtfsIndexB.serviceDatesByServiceId[trip.serviceId]?.dates?.contains(startDateLocalDate) == true
+                    && stopTimes.first().stopId in stopTimesInB.first().first
+                    && stopTimes.first().departureTime == stopTimesInB.first().second
+        }?.tripId
+    }
+}
