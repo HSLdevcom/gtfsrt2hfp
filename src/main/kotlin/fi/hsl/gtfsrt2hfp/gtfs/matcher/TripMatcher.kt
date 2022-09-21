83a4fd81ed0d4c7c818089839fe3ec33d300fbc6
Jaakko Malkki <Jaakko.Malkki@hsl.fi>
2022-09-21 14:19:56 +0300

    Match trips if they have at least 3 stops with same schedule instead of comparing only first stop
    This allows stop times in GTFS feeds to be not exactly the same

--- src/main/kotlin/fi/hsl/gtfsrt2hfp/gtfs/matcher/TripMatcher.kt
+++ src/main/kotlin/fi/hsl/gtfsrt2hfp/gtfs/matcher/TripMatcher.kt
@@ -1,33 +1,48 @@
 package fi.hsl.gtfsrt2hfp.gtfs.matcher
 
 import fi.hsl.gtfsrt2hfp.gtfs.utils.GtfsIndex
-import java.time.LocalDate
-import java.time.format.DateTimeFormatter
+import xyz.malkki.gtfs.model.StopTime
+import xyz.malkki.gtfs.utils.GtfsDateFormat
 
 //TODO: extract interface
 class TripMatcher(private val gtfsIndexA: GtfsIndex, private val gtfsIndexB: GtfsIndex, private val routeMatcher: RouteMatcher) {
-    private val stopCodeStopMatcher = StopCodeStopMatcher(gtfsIndexA, gtfsIndexB)
+    companion object {
+        private val NUM_SAME_STOP_TIMES = 3 //TODO: make this configurable
+    }
+
+    private val stopMatcher = StopCodeStopMatcher(gtfsIndexA, gtfsIndexB)
 
     fun matchTrip(tripId: String, startDate: String): String? {
         val trip = gtfsIndexA.tripsById[tripId] ?: return null
 
-        val stopTimes = gtfsIndexA.stopTimesByTripId[trip.tripId]
-        val stopTimesInB = stopTimes?.mapNotNull { stopTime ->
-            val stopIdB = stopCodeStopMatcher.matchStop(stopTime.stopId).toSet()
-            if (stopIdB.isEmpty()) { null } else { stopIdB to stopTime.departureTime }
-        } ?: emptyList()
-
         val routeIdA = trip.routeId
         val routeIdB = routeMatcher.matchRoute(routeIdA).firstOrNull()
 
-        val startDateLocalDate = LocalDate.parse(startDate, DateTimeFormatter.ofPattern("yyyyMMdd"))
+        val startDateLocalDate = GtfsDateFormat.parseFromString(startDate)
 
-        return gtfsIndexB.tripsById.values.find { trip ->
-            val stopTimes = gtfsIndexB.stopTimesByTripId[trip.tripId]!!
-            return@find trip.routeId == routeIdB
-                    && gtfsIndexB.serviceDatesByServiceId[trip.serviceId]?.dates?.contains(startDateLocalDate) == true
-                    && stopTimes.first().stopId in stopTimesInB.first().first
-                    && stopTimes.first().departureTime == stopTimesInB.first().second
+        val stopTimesA = gtfsIndexA.stopTimesByTripId[trip.tripId] ?: return null
+
+        return gtfsIndexB.tripsById.values.find { tripB ->
+            val stopTimesB = gtfsIndexB.stopTimesByTripId[tripB.tripId]!!
+            return@find tripB.routeId == routeIdB
+                    && gtfsIndexB.serviceDatesByServiceId[tripB.serviceId]?.dates?.contains(startDateLocalDate) == true
+                    && fuzzyIsSameTrip(stopTimesA, stopTimesB)
         }?.tripId
+    }
+
+    private fun fuzzyIsSameTrip(stopTimesA: Collection<StopTime>, stopTimesB: Collection<StopTime>): Boolean {
+        var matchedCount = 0
+
+        stopTimesA.forEach { stopTimeA ->
+            val matchFound = stopTimesB.find { stopTimeB ->
+                (stopTimeA.arrivalTime == stopTimeB.arrivalTime || stopTimeA.departureTime == stopTimeB.departureTime) && stopMatcher.matchStop(stopTimeA.stopId).contains(stopTimeB.stopId)
+            } != null
+
+            if (matchFound && ++matchedCount >= NUM_SAME_STOP_TIMES) {
+                return true
+            }
+        }
+
+        return false
     }
 }
